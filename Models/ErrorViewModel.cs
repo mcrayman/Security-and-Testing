3c21ade98c9eb8729df34f87b47ccf88be5c7d02
Matt McMeans <mrmcmans@memphis.edu>
2024-03-04 19:36:48 -0600

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Security_and_Testing.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
