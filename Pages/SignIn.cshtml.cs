f4a498197ae692147817a2aff64b1dc290129c57
Matt McMeans <mrmcmans@memphis.edu>
2024-04-08 20:38:25 -0500

    Initial commit

---
+++ Pages/SignIn.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWebApp.Pages
+{
+    public class SignInModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
