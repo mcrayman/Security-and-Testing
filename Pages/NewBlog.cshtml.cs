1bfd4dd5f2b5f99f6dfe08d6476d14dec91a6bb0
Matt McMeans <mrmcmans@memphis.edu>
2024-04-08 22:08:01 -0500

    create new blog post page

---
+++ Pages/NewBlog.cshtml.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MyWebApp.Pages
+{
+    public class NewBlogModel : PageModel
+    {
+        [BindProperty]
+        public string Title { get; set; }
+        [BindProperty]
+        public string Content { get; set; }
+        [BindProperty]
+        public IFormFile Image { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var imagePath = Path.Combine("images", Image.FileName);
+            var absoluteImagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", imagePath);
+
+            // Ensuring the images directory exists
+            Directory.CreateDirectory(Path.GetDirectoryName(absoluteImagePath));
+
+            using (var fileStream = new FileStream(absoluteImagePath, FileMode.Create))
+            {
+                await Image.CopyToAsync(fileStream);
+            }
+
+            // Ensuring thread-safety and integrity of the BlogPosts.txt
+            lock (this)
+            {
+                var blogPostDetails = $"{Title}||{Content}||{imagePath}\n";
+                System.IO.File.AppendAllText("BlogPosts.txt", blogPostDetails);
+            }
+
+            return RedirectToPage("/Blog"); // Redirect to Blog page to see the post
+        }
+    }
+}
